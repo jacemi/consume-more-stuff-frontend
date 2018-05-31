fea4f68b553fb3cef989a5a8ffd183343acb522c
Ki Jung Kim <kimkijung@icloud.com>
2018-05-30 15:55:38 -1000

    added header component and logo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
