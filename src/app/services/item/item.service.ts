3ebc38437486a7c01185a7580b780e3b774ddcf4
Ki Jung Kim <kimkijung@icloud.com>
2018-06-01 11:32:37 -1000

    moved some methods

--- src/app/services/item/item.service.ts
+++ src/app/services/item/item.service.ts
@@ -5,31 +5,10 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ItemService {
-  items: Array<any>;
-  item: Object = {
-    id: null,
-    description: '',
-    dimensions: '',
-    manufacturer: '',
-    model: '',
-    price: '',
-    category_id: null,
-    condition_id: null,
-    status_id: null,
-    poster_id: null,
-    category: {},
-    condition: {},
-    photos: [],
-    poster: {},
-    status: {}
-  };
-  // categories: Array<any>;
-  // conditions: Array<any>;
 
   constructor(
     private http: HttpClient
-  ) {
-  }
+  ) { }
 
   fetchAllItems() {
     return this.http.get('/api/items')
@@ -49,8 +28,6 @@ export class ItemService {
   }
 
   publishItem(data) {
-    console.log('Publishing item...');
-    console.log(data);
     return this.http.post('/api/items', data)
       .toPromise()
       .then((item) => {
@@ -82,10 +59,10 @@ export class ItemService {
       .catch((err) => {
         throw err;
       });
-    }
+  }
 
-    editItemById(id, data) {
-      return this.http.put(`/api/items/${id}`, data)
+  editItemById(id, data) {
+    return this.http.put(`/api/items/${id}`, data)
       .toPromise()
       .then((item) => {
         if (!item) {
@@ -99,39 +76,5 @@ export class ItemService {
       .catch((err) => {
         throw err;
       });
-    }
+  }
 
-    fetchCategories() {
-      return this.http.get('/api/categories')
-      .toPromise()
-      .then((categories) => {
-        if (!categories) {
-          const error = new Error();
-          error['status'] = 500;
-          throw error;
-        }
-
-        return categories;
-      })
-      .catch((err) => {
-        throw err;
-      });
-    }
-
-    fetchConditions() {
-      return this.http.get('/api/conditions')
-      .toPromise()
-      .then((conditions) => {
-        if (!conditions) {
-          const error = new Error();
-          error['status'] = 500;
-          throw error;
-        }
-
-        return conditions;
-      })
-      .catch((err) => {
-        throw err;
-      });
-    }
-}
