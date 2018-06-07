f6e19914415fd50697649e1253ae074dc009440a
Ki Jung Kim <kimkijung@icloud.com>
2018-06-06 19:34:40 -1000

    added all page with button on sidebar

--- src/app/pages/all-items/all-items.component.ts
+++ src/app/pages/all-items/all-items.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 
-import { ItemService } from '../../services/item/item.service';
+import { CategoriesService } from '../../services/categories/categories.service';
 
 @Component({
   templateUrl: './all-items.component.html',
   styleUrls: ['./all-items.component.scss']
 })
 export class AllItemsComponent {
-
   constructor(
-    private itemService: ItemService
+    private categoriesService: CategoriesService
   ) {
+    this.categoriesService.fetchAllItemsByCategories()
+      .then(() => {
+
+        this.categoriesWithItems = this.categoriesService.categoriesWithItems;
+      });
   }
+  categoriesWithItems: Object = {
+    categories: []
+  };
+
+
 }
