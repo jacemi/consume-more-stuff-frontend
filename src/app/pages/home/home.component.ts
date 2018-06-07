05f4996a272f0524d36805d2218519e4a50a04cf
Ki Jung Kim <kimkijung@icloud.com>
2018-06-06 15:53:46 -1000

    added all items component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -17,15 +17,5 @@ export class HomeComponent {
     this.categories = this.categoriesService.topItemsByCategories;
   }
   categories: Object = {};
-}
-@Pipe({
-  name: 'search'
-})
-export class SearchPipe implements PipeTransform {
-  public transform(value, keys: string, term: string) {
 
-    if (!term) { return value; }
-    return (value || []).filter((item) => keys.split(',').some(key => item.hasOwnProperty(key) && new RegExp(term, 'gi').test(item[key])));
-
-  }
 }
