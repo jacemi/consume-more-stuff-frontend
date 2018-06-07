91563169977c239982c43e9b06f4d430f3ced0fe
Jace Mitsuka <PJace@BigMac.local>
2018-06-06 14:42:03 -1000

    created ItemFilterPipe to return filtered results based on search input

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Pipe, PipeTransform } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { CategoriesService } from '../../services/categories/categories.service';
@@ -17,4 +17,15 @@ export class HomeComponent {
     this.categories = this.categoriesService.topItemsByCategories;
   }
   categories: Object = {};
+}
+@Pipe({
+  name: 'search'
+})
+export class SearchPipe implements PipeTransform {
+  public transform(value, keys: string, term: string) {
+
+    if (!term) { return value; }
+    return (value || []).filter((item) => keys.split(',').some(key => item.hasOwnProperty(key) && new RegExp(term, 'gi').test(item[key])));
+
+  }
 }
